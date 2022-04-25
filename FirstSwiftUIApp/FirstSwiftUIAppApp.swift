b33aa717707b84dd5023522a3f9c0a8d97f487c0
licon4812 <jalickolli@gmail.com>
2022-04-25 14:32:09 +1000

    Created the ability to favourite landmarks and filter the list for favourites only

--- FirstSwiftUIApp/FirstSwiftUIAppApp.swift
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct FirstSwiftUIAppApp: App {
+    @StateObject private var modelData = ModelData()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
